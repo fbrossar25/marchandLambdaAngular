bc8d92296ab04226c88edf82a9e96a6abfa38edd
brossard <florian.brossard@etu.unistra.fr>
2019-03-16 21:50:44 +0100

    panel admin

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ require_once 'bootstrap-doctrine.php';
 session_start();
 
 $clientRepository = $entityManager->getRepository('Client');
+$articleRepository = $entityManager->getRepository('Article');
 
 $LOADER = new Twig_Loader_Filesystem('src/templates/');
 $TWIG = new Twig_Environment($LOADER, array(
@@ -15,7 +16,7 @@ $TWIG = new Twig_Environment($LOADER, array(
 ));
 $TWIG->addGlobal('session',$_SESSION);
 
-function load_template($template){
+function loadTemplate($template){
     global $TWIG;
     return $TWIG->loadTemplate("$template.twig.html");
 }
@@ -23,6 +24,11 @@ function load_template($template){
 function emailDisponible($email){
     global $clientRepository;
     return $clientRepository->count(['email' => $email]) === 0;
+}
+
+function articleDisponible($article){
+    global $articleRepository;
+    return $articleRepository->count(['nom' => $article]) === 0;
 }
 
 function connect($email, $pass){
@@ -35,7 +41,7 @@ function connect($email, $pass){
     }
 }
 
-function enregistrer_utilisateur(array $data){
+function enregistrerUtilisateur(array $data){
     global $entityManager;
     $client = new Client();
     $client->setPrenom($data['firstName']);
@@ -47,13 +53,36 @@ function enregistrer_utilisateur(array $data){
     $client->setNumeroVoie($data['streetNo']);
     $client->setTelephone($data['phoneNumber']);
     $client->setCodePostal($data['postalCode']);
-    try{
-        $entityManager->persist($client);
-        $entityManager->flush();
-    }catch (\Doctrine\ORM\OptimisticLockException | \Doctrine\ORM\ORMException $e) {
+    if(emailDisponible($client->getEmail())){
+        try{
+            $entityManager->persist($client);
+            $entityManager->flush();
+        }catch (\Doctrine\ORM\OptimisticLockException | \Doctrine\ORM\ORMException $e) {
+            return false;
+        }
+        return $client;
+    }else{
         return false;
     }
-    return $client;
+}
+
+function ajouterArticle(array $data){
+    global $entityManager;
+    $article = new Article();
+    $article->setNom($data['name']);
+    $article->setDescription($data['description']);
+    $article->setPrix($data['price']);
+    if(articleDisponible($article->getNom())){
+        try {
+            $entityManager->persist($article);
+            $entityManager->flush();
+            return true;
+        } catch (\Doctrine\ORM\ORMException $e) {
+            return false;
+        }
+    }else{
+        return false;
+    }
 }
 
 $app = new \Slim\App([
@@ -63,7 +92,7 @@ $app = new \Slim\App([
     'notFoundHandler' => function ($c) {
         return function ($req, $resp) use ($c) {
             $response = new \Slim\Http\Response(404);
-            return $response->write(load_template('404')->render([]));
+            return $response->write(loadTemplate('404')->render([]));
         };
     }
 ]);
@@ -71,11 +100,11 @@ $app = new \Slim\App([
 $app->redirect('/', '/accueil', 303);
 
 $app->get('/accueil', function(Request $req, Response $resp, array $args){
-    return $resp->write(load_template('accueil')->render([]));
+    return $resp->write(loadTemplate('accueil')->render([]));
 });
 
 $app->get('/connexion', function(Request $req, Response $resp, array $args){
-    return $resp->write(load_template('connexion')->render([]));
+    return $resp->write(loadTemplate('connexion')->render([]));
 });
 
 $app->post('/connexion', function(Request $req, Response $resp, array $args){
@@ -86,7 +115,7 @@ $app->post('/connexion', function(Request $req, Response $resp, array $args){
         $_SESSION['admin'] = $client->getAdmin();
         header('Refresh:0, url=/accueil');
     }else{
-        return $resp->write(load_template('connexion')->render([
+        return $resp->write(loadTemplate('connexion')->render([
             'erreur' => true,
             'email' => $data['email']
         ]));
@@ -96,23 +125,51 @@ $app->post('/connexion', function(Request $req, Response $resp, array $args){
 $app->get('/deconnexion', function(Request $req, Response $resp, array $args){
     unset($_SESSION['email']);
     unset($_SESSION['admin']);
-    header('Refresh:0; url=/accueil');
+    return $resp->withRedirect('/accueil');
 });
 
 $app->get('/inscription', function(Request $req, Response $resp, array $args){
-    return $resp->write(load_template('inscription')->render([]));
+    return $resp->write(loadTemplate('inscription')->render([]));
 });
 
 $app->post('/inscription', function(Request $req, Response $resp, array $args){
-    $client = enregistrer_utilisateur($req->getParsedBody());
+    $client = enregistrerUtilisateur($req->getParsedBody());
     if($client !== null && $client !== false){
         $_SESSION['email'] = $client->getEmail();
-        header('Refresh:0; url=/accueil');
+        return $resp->withRedirect('/accueil');
     }else{
-        return $resp->write(load_template('erreur')->render([
+        return $resp->write(loadTemplate('erreur')->render([
             'message'=>"Erreur lors de l'inscription, veuillez ré-essayer ultérieurement"
         ]));
     }
+});
+
+$app->get('/administration', function(Request $req, Response $resp, array $args){
+    if(isset($_SESSION['admin']) && $_SESSION['admin']){
+        error_log(''.implode($args).' -- '.implode($req->getParsedBody() === null ? [] : $req->getParsedBody() , ','));
+        return $resp->write(loadTemplate('administration')
+                    ->render($req->getParsedBody() === null ? [] : $req->getParsedBody()));
+    }else{
+        return $resp->write(loadTemplate('erreur')->render([
+            'message' => "Vous n'avez pas l'autorisation d'accéder à cette page"
+        ]));
+    }
+})->setName('administration');
+
+$app->post('/ajouter-article', function(Request $req, Response $resp, array $args){
+    if($_SESSION['admin']){
+        $added = ajouterArticle($req->getParsedBody());
+        return $resp->withRedirect($this->router->pathFor('administration',
+            ['added' => $added], ['added' => $added]));
+    }else{
+        return $resp->write(loadTemplate('erreur')->render([
+            'message' => "Vous n'avez pas l'autorisation d'accéder à cette page"
+        ]));
+    }
+});
+
+$app->get('/article-disponible', function(Request $req, Response $resp, array $args){
+    return $resp->withJson(articleDisponible($req->getParam('name')));
 });
 
 $app->get('/email-disponible', function(Request $req, Response $resp, array $args){
---
+++ src/assets/js/administration.js
@@ -0,0 +1,28 @@
+$('#error').hide();
+
+$.validator.messages.required = 'Ce champ est requis';
+
+$('#subscriptionForm').validate({
+    rules:{
+        name:{
+            remote: 'article-disponible'
+        }
+    },
+    messages:{
+        price:{
+            min: 'Vous devez saisir un nombre > 0'
+        },
+        name:{
+            remote: 'Ce produit existe déjà'
+        }
+    },
+    invalidHandler: (event, validator) => {
+        if(validator.numberOfInvalids()){
+            $('#error').show();
+        }else{
+            $('#error').hide();
+        }
+    },
+    errorClass: 'is-invalid',
+    validClass: 'is-valid'
+});
